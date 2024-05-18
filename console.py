1d2f359e3da269187ac98d45eea1a871b7055c94
smoxhakim <joulal1994@gmail.com>
2024-05-18 22:59:47 +0100

    Bag_Fix_0.1.8

--- console.py
+++ console.py
@@ -6,7 +6,11 @@ import ast
 
 from models.base_model import BaseModel
 from models.user import User
-
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 from models.engine.file_storage import FileStorage
 
